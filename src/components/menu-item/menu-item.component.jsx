b1238bc7affe3d8dc8a9539d44ba724fdeec79b5
saleem278 <saleem27898@gmail.com>
2022-02-01 17:07:16 +0530

    "commit"

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,22 +1,30 @@
 import React from "react";
-// import {withRouter} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size,history,linkUrl,match }) => (
-  <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
+const MenuItem = ({ title, imageUrl, size,  linkUrl }) => {
+  let navigate = useNavigate();
+  return (
     <div
-      className="background-image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
+      className={`${size} menu-item`}
+      onClick={() => {
+        navigate(`${linkUrl}`);
       }}
-    />
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
 
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MenuItem;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,18 @@
 import React, { Component } from "react";
-import SHOP_DATA from "./shop.data";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-class Shop extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherprops }) => (
-          <CollectionPreview key={id} {...otherprops} />
-        ))}
-      </div>
-    );
-  }
-}
-export default Shop;
+import { selectCollections } from "../../redux/shop/shop.selectors";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+const Shop = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...otherprops }) => (
+      <CollectionPreview key={id} {...otherprops} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+collections:selectCollections
+})
+export default connect(mapStateToProps)(Shop);
