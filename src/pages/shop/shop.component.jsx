a3e440c6ee6f7ce4e430efd4377a15a7f074c2c0
saleem278 <saleem27898@gmail.com>
2022-02-01 17:35:09 +0530

    commit

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,18 +1,10 @@
-import React, { Component } from "react";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-import { selectCollections } from "../../redux/shop/shop.selectors";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import React from "react";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-const Shop = ({ collections }) => (
+const Shop = () => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherprops }) => (
-      <CollectionPreview key={id} {...otherprops} />
-    ))}
+    <CollectionsOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-collections:selectCollections
-})
-export default connect(mapStateToProps)(Shop);
+export default Shop;
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,7 +1,7 @@
 import { createSelector } from "reselect";
-const selectShop = (state) => state.shop;
+const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
-  (shop) => shop.Collections
+  (shop) => shop.collections
 );
