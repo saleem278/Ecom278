13819e4ddf4bc523ed290a24929eea7c1a7b618f
saleem278 <saleem27898@gmail.com>
2022-02-09 10:50:06 +0530

    commit

---
+++ client/src/components/Error/Error.component.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import {
+  ErrorImageOverlay,
+  ErrorImageContainer,
+  ErrorImageText,
+} from "./Error.styles";
+
+class ErrorBoundary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hasErrored: false,
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    // process the error
+    return { hasErrored: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error);
+  }
+
+  render() {
+    if (this.state.hasErrored) {
+      return (
+        <ErrorImageOverlay>
+          <ErrorImageContainer/>
+          <ErrorImageText>Something Went Wrong</ErrorImageText>
+        </ErrorImageOverlay>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
