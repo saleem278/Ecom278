5815a1e0a25126d72fe401ee782e77a8c615f133
saleem278 <saleem27898@gmail.com>
2022-02-09 12:48:46 +0530

    commit

--- server.js
+++ server.js
@@ -1,42 +1,48 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const compression = require("compression");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const enforce = require('express-sslify');
 
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
-
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
-app.post("/payment", (req, res) => {
+app.listen(port, error => {
+  if (error) throw error;
+  console.log('Server running on port ' + port);
+});
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
+
+app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
-    currency: "inr",
+    currency: 'usd'
   };
-  stripe.charges.create(body, (strErr, strRes) => {
-    if (strErr) {
-      res.status(500).send({ error: strErr });
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
     } else {
-      res.status(200).send({ success: strRes });
+      res.status(200).send({ success: stripeRes });
     }
   });
-});
-
-app.listen(port, (error) => {
-  if (error) throw error;
-  console.log("Server running on " + port);
 });
