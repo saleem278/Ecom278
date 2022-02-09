ce05f7cf37326659b38f8d5fc3a61399a9147501
saleem278 <saleem27898@gmail.com>
2022-02-09 12:58:28 +0530

    commit

--- server.js
+++ server.js
@@ -14,14 +14,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-
 app.listen(port, error => {
   if (error) throw error;
   console.log('Server running on port ' + port);
@@ -30,6 +22,14 @@ app.listen(port, error => {
 app.get('/service-worker.js', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.post('/payment', (req, res) => {
   const body = {
