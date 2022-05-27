564b725d1051deef73ceb6257a3a2dc0096cf1e7
saleem278 <saleem27898@gmail.com>
2022-05-27 15:26:53 +0530

    last commit

--- client/src/redux/directory/directory.reducer.js
+++ client/src/redux/directory/directory.reducer.js
@@ -3,33 +3,33 @@ const INITIAL_STATE = {
     {
       title: 'Electronics',
       imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ecom78-129c2.appspot.com/o/Samsung%20Smart%20TV.webp?alt=media&token=22247f13-98b4-4dbe-9bc6-86a6569a44ac',
-      id: 1,
+      id: 0,
       linkUrl: 'shop/electronics'
     },
     {
       title: 'Clothes',
       imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-      id: 2,
+      id: 1,
       linkUrl: 'shop/clothes'
     },
     {
       title: 'Grocery',
       imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ecom78-129c2.appspot.com/o/Grocery%20Banner.jpg?alt=media&token=e9638833-4d0a-4f43-a4d2-12af053e5d0f',
-      id: 3,
+      id: 2,
       linkUrl: 'shop/grocery'
     },
     {
       title: 'Beauty and Personal Care',
       imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
       size: 'large',
-      id: 4,
-      linkUrl: 'shop/beauty'
+      id: 3,
+      linkUrl: 'shop/beauty and personal care'
     },
     {
       title: 'Books',
       imageUrl: 'https://firebasestorage.googleapis.com/v0/b/ecom78-129c2.appspot.com/o/Book%20Banner.jpg?alt=media&token=3c47cdd0-6818-476f-a67c-92d32f124152',
       size: 'large',
-      id: 5,
+      id: 4,
       linkUrl: 'shop/books'
     }
   ]
